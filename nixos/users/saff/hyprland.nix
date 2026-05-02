b3e2129ad73b0360c19999a929cb55189a5eea7a
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-05-02 21:54:06 +0300

    feat(nixos): add wl-clipboard

--- nixos/users/saff/hyprland.nix
+++ nixos/users/saff/hyprland.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }: {
   home.sessionVariables.NIXOS_OZONE_WL = 1;
 
+  home.packages = with pkgs; [
+    wl-clipboard
+  ];
+
   wayland.windowManager.hyprland.systemd.enable = false;
 
   xdg.configFile.hypr = {
