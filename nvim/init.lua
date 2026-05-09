ac1e13cef55417f25114b707eb61143156329811
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-05-09 04:16:09 +0300

    feat(nvim): add nvim-treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,3 +29,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('yank-hl', { clear = true }),
   callback = function() vim.hl.on_yank() end,
 })
+
+vim.pack.add({ 'https://github.com/nvim-treesitter/nvim-treesitter' })
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('ts-start', { clear = true }),
+  callback = function(event)
+    local lang = vim.treesitter.language.get_lang(event.match)
+    if not lang or not vim.treesitter.language.add(lang) then return end
+    vim.treesitter.start(event.buf, lang)
+  end,
+})
