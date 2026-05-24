a9325c647e025ea3b7de97af934ba8b377845367
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-05-25 01:28:17 +0300

    feat(nixos): wrap yazi and lazygit for auto cd

--- nixos/users/saff/lazygit.nix
+++ nixos/users/saff/lazygit.nix
@@ -1,6 +1,17 @@
 { config, pkgs, ... }: {
   home.packages = [ pkgs.lazygit ];
 
+  programs.bash.bashrcExtra = ''
+    function lg() {
+      export LAZYGIT_NEW_DIR_FILE=~/.lazygit/newdir
+      command lazygit "$@"
+      if [ -f $LAZYGIT_NEW_DIR_FILE ]; then
+        cd "$(cat $LAZYGIT_NEW_DIR_FILE)"
+        rm -f $LAZYGIT_NEW_DIR_FILE > /dev/null
+      fi
+    }
+  '';
+
   xdg.configFile.lazygit = {
     source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/lazygit";
     recursive = true;
--- nixos/users/saff/yazi.nix
+++ nixos/users/saff/yazi.nix
@@ -1,6 +1,17 @@
 { config, pkgs, ... }: {
   home.packages = [ pkgs.yazi ];
 
+  programs.bash.bashrcExtra = ''
+    function y() {
+      local tmp="$(mktemp -t "yazi-cwd.XXXXX")"
+      command yazi "$@" --cwd-file="$tmp"
+      if cwd="$(<"$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+        builtin cd -- "$cwd"
+      fi
+      rm -f -- "$tmp"
+    }
+  '';
+
   xdg.configFile.yazi = {
     source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/yazi";
     recursive = true;
