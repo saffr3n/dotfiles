a8112b124f923886710e4c6dc97810e382e53b76
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-06-01 21:52:52 +0300

    feat(gtk): add configs

--- nixos/users/saff/noctalia.nix
+++ nixos/users/saff/noctalia.nix
@@ -17,4 +17,7 @@ in {
 
   xdg.configFile."noctalia/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/noctalia/settings.json";
   xdg.configFile."qt6ct/qt6ct.conf".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/qt6ct/qt6ct.conf";
+  xdg.configFile."nwg-look/config".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/nwg-look/config";
+  xdg.configFile."gtk-3.0/settings.ini".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/gtk-3.0/settings.ini";
+  xdg.configFile."gtk-4.0/settings.ini".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/gtk-4.0/settings.ini";
 }
