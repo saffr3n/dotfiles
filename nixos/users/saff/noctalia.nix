debd8387ff673eda87b4a7b61c689782b86b3c99
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-06-01 21:45:19 +0300

    feat(noctalia): add config

--- nixos/users/saff/noctalia.nix
+++ nixos/users/saff/noctalia.nix
@@ -3,4 +3,6 @@
     noctalia-shell
     papirus-icon-theme
   ];
+
+  xdg.configFile."noctalia/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/noctalia/settings.json";
 }
