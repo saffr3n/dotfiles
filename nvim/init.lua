db7a166b44f93f6a02e19619d124b876fcf12bcc
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-06-03 19:05:27 +0300

    feat(nvim): configure netrw

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,7 +16,13 @@ vim.keymap.set('n', ']t', vim.cmd.tabnext)
 vim.keymap.set('n', '<Esc>', vim.cmd.nohlsearch)
 vim.keymap.set('n', '<Leader>q', vim.diagnostic.setqflist)
 vim.keymap.set('n', '<Leader>l', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<Leader>e', ':20Lex<CR>')
 
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_sizestyle = 'H'
+vim.g.netrw_altfile = 1
+vim.g.netrw_browse_split = 4
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.signcolumn = 'yes'
