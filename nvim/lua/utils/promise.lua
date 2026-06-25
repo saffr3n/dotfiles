2ebd5e4c552a8cbfc3355a2031fec0ce87d33cae
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-06-25 22:24:24 +0300

    feat(nvim): add simplified promise-like logic
    This doesn't do any async work for now and just resolves/rejects
    instantly, but it lays some ground work to base the actual
    implementation off of.

--- nvim/lua/utils/promise.lua
+++ nvim/lua/utils/promise.lua
@@ -1,12 +1,33 @@
 local M = {}
+local H = {}
 
-function M.new()
+function M.new(executor)
   local self = {
     status = 'pending',
     value = nil,
   }
 
+  if executor then
+    local resolve = function(value) H.resolve(self, value) end
+    local reject = function(cause) H.reject(self, cause) end
+    local ok, err = H.try(executor, resolve, reject)
+    if not ok then reject(err) end
+  end
+
   return self
+end
+
+function H.resolve(promise, value) H.settle(promise, 'resolved', value) end
+function H.reject(promise, cause) H.settle(promise, 'rejected', cause) end
+
+function H.settle(promise, status, value)
+  if promise.status ~= 'pending' then return end
+  promise.status = status
+  promise.value = value
+end
+
+function H.try(fn, ...)
+  return xpcall(fn, function(err) return debug.traceback(err, 2) end, ...)
 end
 
 return M
