0f2db0c5a68f6cb8a0f83498441f56c96824c7ef
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-07-07 14:53:22 +0300

    fix(hypr): remove unneeded hyprland-run windowrule

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -218,11 +218,3 @@ hl.window_rule({
   },
   no_focus = true,
 })
-
--- Hyprland-run windowrule
-hl.window_rule({
-  name  = 'move-hyprland-run',
-  match = { class = 'hyprland-run' },
-  move  = '20 monitor_h-120',
-  float = true,
-})
