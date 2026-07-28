74998a10d5fbdd2352c3997a95bf536bbb07f146
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-07-29 02:01:59 +0300

    fix(nixos): use correct noctalia config file name

--- nixos/users/saff/noctalia.nix
+++ nixos/users/saff/noctalia.nix
@@ -15,7 +15,7 @@ in {
     };
   };
 
-  xdg.configFile."noctalia/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/noctalia/config.toml";
+  xdg.configFile."noctalia/config.toml".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/noctalia/config.toml";
   xdg.configFile."qt6ct/qt6ct.conf".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/qt6ct/qt6ct.conf";
   xdg.configFile."nwg-look/config".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/nwg-look/config";
   xdg.configFile."gtk-3.0/settings.ini".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/gtk-3.0/settings.ini";
