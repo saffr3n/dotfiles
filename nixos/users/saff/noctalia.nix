f886b03464bd73eb253d7c92343b6d18c7ec09b0
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-07-29 01:03:45 +0300

    feat(noctalia): move to v5

--- nixos/users/saff/noctalia.nix
+++ nixos/users/saff/noctalia.nix
@@ -3,7 +3,7 @@
 in {
   home = {
     packages = with pkgs; [
-      noctalia-shell
+      noctalia
       qt6Packages.qt6ct
       nwg-look
       adw-gtk3
@@ -15,7 +15,7 @@ in {
     };
   };
 
-  xdg.configFile."noctalia/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/noctalia/settings.json";
+  xdg.configFile."noctalia/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/noctalia/config.toml";
   xdg.configFile."qt6ct/qt6ct.conf".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/qt6ct/qt6ct.conf";
   xdg.configFile."nwg-look/config".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/nwg-look/config";
   xdg.configFile."gtk-3.0/settings.ini".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/gtk-3.0/settings.ini";
