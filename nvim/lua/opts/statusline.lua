6b93cc2457caca6fdbc46586c5df5edc2ab0fee4
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-08-28 00:48:45 +0300

    feat(nvim): format diagnostics for statusline

--- nvim/lua/opts/statusline.lua
+++ nvim/lua/opts/statusline.lua
@@ -1,4 +1,5 @@
-local o = vim.o
+local o   = vim.o
+local api = vim.api
 
 o.statusline = '%!v:lua.StatusLine()'
 o.laststatus = 3
@@ -8,7 +9,7 @@ o.showmode   = false
 vim.g.qf_disable_statusline = 1
 
 local function replace_keycode(keycode)
-  return vim.api.nvim_replace_termcodes(keycode, true, false, true)
+  return api.nvim_replace_termcodes(keycode, true, false, true)
 end
 
 local modes = setmetatable({
@@ -29,11 +30,31 @@ local modes = setmetatable({
   end,
 })
 
+local diag_lvls = {
+  { text = 'E', hl = '%#StatusLineDiagnosticError#' },
+  { text = 'W', hl = '%#StatusLineDiagnosticWarn#'  },
+  { text = 'I', hl = '%#StatusLineDiagnosticInfo#'  },
+  { text = 'H', hl = '%#StatusLineDiagnosticHint#'  },
+}
+
 local info_hl = '%#StatusLineInfo#'
+
+local function section_diag(buf)
+  local count = vim.diagnostic.count(buf)
+  if vim.tbl_isempty(count) then return end
+  local parts = {}
+  for i, lvl in ipairs(diag_lvls) do
+    local c = count[i]
+    if c then
+      table.insert(parts, ' ' .. lvl.hl .. lvl.text .. c)
+    end
+  end
+  return table.concat(parts)
+end
 
 function _G.StatusLine()
   local parts = {}
-  local buf = vim.api.nvim_get_current_buf()
+  local buf = api.nvim_get_current_buf()
 
   -- mode section
   local mode = modes[vim.fn.mode()]
@@ -43,10 +64,14 @@ function _G.StatusLine()
   local devinfo_parts = { info_hl }
 
   local lsp_count = #vim.lsp.get_clients({ bufnr = buf })
-  if lsp_count > 0 then table.insert(devinfo_parts, ' @' .. lsp_count) end
+  if lsp_count > 0 then
+    table.insert(devinfo_parts, ' @' .. lsp_count)
+  end
 
-  local diag = vim.diagnostic.status(buf)
-  if diag ~= '' then table.insert(devinfo_parts, ' ' .. diag) end
+  local diag = section_diag(buf)
+  if diag then
+    table.insert(devinfo_parts, diag)
+  end
 
   if #devinfo_parts > 1 then
     table.insert(devinfo_parts, ' ')
