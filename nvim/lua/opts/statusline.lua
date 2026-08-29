41781cf22a9cd1fad179f25969c8667e737d380a
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-08-30 00:31:56 +0300

    feat(nvim): cache statusline diagnostic counts

--- nvim/lua/opts/statusline.lua
+++ nvim/lua/opts/statusline.lua
@@ -11,7 +11,7 @@ o.showmode   = false
 
 vim.g.qf_disable_statusline = 1
 
----@type table<integer, { lsp_count: string }?>
+---@type table<integer, { lsp_count: string, diag_count: string }?>
 local state   = {}
 local group   = api.nvim_create_augroup('saff.statusline', { clear = true })
 
@@ -42,19 +42,6 @@ local diag_lvls = {
 
 local info_hl = '%#StatusLineInfo#'
 
-local function section_diag(buf)
-  local count = vim.diagnostic.count(buf)
-  if vim.tbl_isempty(count) then return end
-  local parts = {}
-  for i, lvl in ipairs(diag_lvls) do
-    local c = count[i]
-    if c then
-      table.insert(parts, ' ' .. lvl.hl .. lvl.text .. c)
-    end
-  end
-  return table.concat(parts)
-end
-
 function _G.StatusLine()
   local parts = {}
   local buf = api.nvim_get_current_buf()
@@ -69,11 +56,7 @@ function _G.StatusLine()
   local s = state[buf]
   if s then
     table.insert(devinfo_parts, s.lsp_count)
-  end
-
-  local diag = section_diag(buf)
-  if diag then
-    table.insert(devinfo_parts, diag)
+    table.insert(devinfo_parts, s.diag_count)
   end
 
   if #devinfo_parts > 1 then
@@ -108,6 +91,25 @@ local function update_lsp_count(buf)
   end)
 end
 
+---@param buf integer
+local function update_diag_count(buf)
+  local s = state[buf]
+  if not s then return end
+  
+  local count = vim.diagnostic.count(buf) ---@as [integer?, integer?, integer?, integer?]
+  local parts = {}
+
+  for i, lvl in ipairs(diag_lvls) do
+    local c = count[i]
+    if c then
+      table.insert(parts, ' ' .. lvl.hl .. lvl.text .. c)
+    end
+  end
+
+  s.diag_count = table.concat(parts)
+  api.nvim__redraw({ buf = buf, statusline = true })
+end
+
 au('BufEnter', {
   group = group,
   callback = function(e)
@@ -118,7 +120,10 @@ au('BufEnter', {
       return
     end
 
-    state[buf] = state[buf] or { lsp_count = '' }
+    state[buf] = state[buf] or {
+      lsp_count  = '',
+      diag_count = '',
+    }
   end,
 })
 
@@ -134,4 +139,11 @@ au({ 'LspAttach', 'LspDetach' }, {
   callback = function(e)
     update_lsp_count(e.buf)
   end
+})
+
+au('DiagnosticChanged', {
+  group = group,
+  callback = function(e)
+    update_diag_count(e.buf)
+  end,
 })
