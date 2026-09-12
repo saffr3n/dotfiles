63f25ecae23ab407cfa804ffc2d72e62701a5420
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-09-12 14:52:57 +0300

    feat(nvim): use default fold open/close fillchars

--- nvim/lua/opts/statuscolumn.lua
+++ nvim/lua/opts/statuscolumn.lua
@@ -8,7 +8,7 @@ o.foldcolumn     = '1'
 o.foldtext       = ''
 o.foldmethod     = 'indent'
 o.foldlevel      = 99
-o.fillchars      = 'fold: ,foldsep: ,foldinner: ,foldopen:v,foldclose:>,eob: '
+o.fillchars      = 'fold: ,foldsep: ,foldinner: ,eob: '
 
 function _G.SaffStatusColumn()
   return '%l%s%C'
